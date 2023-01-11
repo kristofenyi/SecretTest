673dbb822ec0ed8489abdccc5fe784a380906b13
3p1demicz <3p1demicz@gmail.com>
2023-01-11 21:35:50 +0100

    init

--- API-Test/Program.cs
+++ API-Test/Program.cs
@@ -5,7 +5,25 @@ namespace API_Test
     {
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
+            
+                var builder = WebApplication.CreateBuilder(args);
+            //string mySecret = string.Empty;
+            //mySecret = builder.Configuration["secret"];
+            //if (mySecret.Length < 1)
+            //{
+            //    Console.WriteLine("It is empty");
+            //}
+            //else
+            //{
+            //    Console.WriteLine(mySecret);
+            //}
+
+            Console.WriteLine(builder.Configuration["secret"]);
+            Console.WriteLine("Secrete is " + builder.Configuration["secret"]);
+            Console.WriteLine(builder.Configuration["secret"]);
+            Console.WriteLine("secrete under this");
+            Console.WriteLine(builder.Configuration["secret"]);
+            Console.WriteLine(builder.Configuration["secret"]);
 
             // Add services to the container.
 
@@ -13,26 +31,25 @@ namespace API_Test
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-            var mySecret = builder.Configuration["secret"];
-            Console.WriteLine(mySecret);
+            
+                var app = builder.Build();
 
-            var app = builder.Build();
+                // Configure the HTTP request pipeline.
+                if (app.Environment.IsDevelopment())
+                {
+                    app.UseSwagger();
+                    app.UseSwaggerUI();
+                }
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+                app.UseHttpsRedirection();
 
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
+                app.UseAuthorization();
 
 
-            app.MapControllers();
+                app.MapControllers();
 
-            app.Run();
+                app.Run();
+           
         }
     }
 }
